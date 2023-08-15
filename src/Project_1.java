378819a560e097012fc40ab3e6ba783f598f8efb
Apple-User-163 <avinumathew@gmail.com>
2023-08-15 19:40:47 +0530

    From IntelliJ

---
+++ src/Project_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Project_1
+{
+    public static void main(String[] args)
+    {
+        Scanner inpt = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = inpt.nextInt();
+    }
+}
