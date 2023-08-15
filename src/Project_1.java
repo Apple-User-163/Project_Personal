78fb18c361a24be412e3a551aac0bf48e18a95f6
Apple-User-163 <avinumathew@gmail.com>
2023-08-15 19:47:15 +0530

    From IntelliJ

--- src/Project_1.java
+++ src/Project_1.java
@@ -7,5 +7,14 @@ public class Project_1
         System.out.println("Enter the number");
         int num = inpt.nextInt();
 
+        int digit = 0;
+        int temp = num;
+
+        for (int i = 0; i < 3; i++)
+        {
+            digit = temp % 10;
+            temp = temp / 10;
+            System.out.println(digit);
+        }
     }
 }
