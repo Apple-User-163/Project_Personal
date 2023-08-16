cb0f8e8775c78a3218668d12ab648dd30fdab583
Apple-User-163 <avinumathew@gmail.com>
2023-08-16 21:04:07 +0530

    From IntelliJ

--- src/Project_2.java
+++ src/Project_2.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 public class Project_2
 {
+    public static void main(String[] args)
+    {
+        Scanner inpt = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = inpt.nextInt();
+        boolean flag = false;
 
+        int i = 2;
+        for (; i < num; i++)
+        {
+            if (num % i == 0 && num != i && !flag)
+            {
+                System.out.println("The number is a composite number");
+                flag = true;
+            }
+        }
+        if (num <= i && !flag)
+        {
+            System.out.println("The number is not a composite number");
+        }
+    }
 }
